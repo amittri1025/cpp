bbee55fe03c85cfb9a040f0599a302bd504753d3
Amit Tripathi <tripathiamit2021@gmail.com>
2026-02-12 21:40:12 +0530

    done

--- 2-sortings/selection.cpp
+++ 2-sortings/selection.cpp
@@ -16,25 +16,31 @@ void print(int arr[], int size){
 }
 
 void selectionsort(int arr[], int n){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(arr[i]<arr[j]) swap(arr[i], arr[j]);
+    for(int i=0; i<n-1; i++){
+        int smallestidx = i; // suppose first element of the window is min 
+        for(int j=i; j<n; j++){
+            if(arr[j]<arr[smallestidx]) smallestidx = j;
         }
+        swap(arr[i], arr[smallestidx]);// swamp with first element
     }
 }
 
-
+// void bubblesort(int arr[], int n){
+//     int sort = 0; 
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             if(arr[i]<arr[j]) swap(arr[i], arr[j]);
+//             else sort = 1
+//         }
+//     }
+// }
 
 int main(){
     int arr[] = {3,23,231,9,2,4};
     int size = sizeof(arr)/sizeof(arr[0]);
-
-   print(arr, size);
-
+    print(arr, size);
     cout<<endl<<"after operation"<<endl;
-
     selectionsort(arr, size);
-
     print(arr, size);
     return 0 ; 
 }
